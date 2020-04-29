c7185d84b5650a6b41af6b3fca1991391c31f286
bevarb <775374937@qq.com>
2020-04-29 22:51:05 +0800

    add trainval.txt

--- get_txt.py
+++ get_txt.py
@@ -1,11 +1,16 @@
 import os
 import random
-list = os.listdir("Annotation")
+list = os.listdir("Annotations")
 random.shuffle(list)
 len = len(list)
-train = list[:int(0.8 * len)]
-test = list[int(0.8 * len):int(0.95 * len)]
-val = list[int(0.95 * len):len]
+trainval = list[:int(0.9 * len)]
+
+train_val_all = trainval.copy()
+random.shuffle(train_val_all)
+train = train_val_all[:int(0.8 * len)]
+val = train_val_all[int(0.8 * len):int(0.9 * len)]
+
+test = list[int(0.9 * len):len]
 def get_txt(list, name):
     if not os.path.exists(name + ".txt"):
         os.mknod(name + ".txt")
@@ -13,6 +18,7 @@ def get_txt(list, name):
     for i in list:
         f1.write(i.split(".")[0] + "\n")
     f1.close()
+get_txt(trainval, "trainval")
 get_txt(train, "train")
 get_txt(test, "test")
 get_txt(val, "val")
